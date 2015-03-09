6bace9027c934b5015997e9a414441db133a1561
Wilhelm Eklund <wille.eklund@gmail.com>
2015-03-09 21:19:03 +0100

    Initial Commit

---
+++ Hinner jag?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hinner jag?
+//
+//  Created by Wilhelm Eklund on 09/03/15.
+//  Copyright (c) 2015 Wilhelm Eklund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
