db10ece42717ad6d38ee1ed5b605a8617fb910c6
Wilhelm Eklund <wilhelm.eklund@postkodlotteriet.se>
2015-04-17 18:06:17 +0200

    Memory-cached version of the API, ttl 20 seconds

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,81 @@ require('colors');
 var express = require('express'),
     path = require('path'),
     request = require('request'),
+    config = require('./config'),
+    async = require('async'),
+    memoryCache = require('memory-cache'),
+
+/**
+ * Fetch data from SL api using asyncronous queue
+ */
+result_queues = {},
+ttl_cache = 20,
+
+getQueueNameFromReqParams = function (params) {
+  return params.site_id;
+},
+
+updateResultCache = function (req, res, callback) {
+  var realtimeKey = 'bebfe14511a74ca5aef16db943ae8589',
+  timewindow = 30,
+  SL_api_url = 'http://api.sl.se/api2/realtimedepartures.json?' +
+               'key=' + realtimeKey +
+               '&timewindow=' + timewindow +
+               '&siteid=' + req.params.site_id;
+
+  var queueName = getQueueNameFromReqParams(req.params);
+
+  request(SL_api_url, function (err, requestResult) {
+    if (err) {
+      console.log('Error requesting from SL'.red, err);
+      console.log('requestResult from SL'.red, requestResult);
+      return;
+    }
+    var content = JSON.parse(requestResult.body);
+    if (
+      undefined === content ||
+      undefined === content.ResponseData ||
+      undefined === content.ResponseData.Metros ||
+      0 === content.ResponseData.Metros.length
+    ) {
+      console.log('Error: no metro departures'.red, ('for siteid ' + req.params.site_id).yellow);
+    }
+    console.log('Result content'.yellow, content);
+    // TODO: Only use content.ResponseData.Metros
+    memoryCache.put(queueName, content, 1000 * ttl_cache);
+    callback(err, content);
+  });
+},
+
+getResultDataFromRequest = function (task, done) {
+  var queueName = getQueueNameFromReqParams(task.req.params);
+  var cachedResultData = memoryCache.get(queueName);
+  if (cachedResultData && config.useMemoryCache) {
+    // Send back cached result
+    console.log('cache hit'.cyan, queueName);
+    task.callback(cachedResultData);
+    done();
+  } else {
+    // Fetch result from SL api
+    console.log('cache miss'.yellow, queueName);
+    updateResultCache(task.req, task.res, function (err, newResultData) {
+      task.callback(newResultData);
+      done();
+    });
+  }
+},
+
+queueResultRequest = function (req, res, callback) {
+  var queueName = getQueueNameFromReqParams(req.params);
+  // Create the queue if it does not exist
+  result_queues[queueName] = result_queues[queueName] || async.queue(getResultDataFromRequest, 1);
+  // Add request to queue
+  result_queues[queueName].push({
+    req: req,
+    res: res,
+    callback: callback
+  });
+},
 
 /**
  * Express application configuration
@@ -28,19 +103,10 @@ app.get('/hej', function (req, res) {
 });
 
 app.get('/api/realtimedepartures/:site_id.json', function (req, res) {
-  var realtimeKey = 'bebfe14511a74ca5aef16db943ae8589',
-  timewindow = 30,
-  SL_api_url = 'http://api.sl.se/api2/realtimedepartures.json?' +
-               'key=' + realtimeKey +
-               '&timewindow=' + timewindow +
-               '&siteid=' + req.params.site_id;
-
-  request(SL_api_url, function (err, res) {
-    if (err) {
-      console.log('Error requesting from SL'.red, err);
-      return;
-    }
-  }).pipe(res)
+  queueResultRequest(req, res, function (result) {
+    res.header('Cache-Control', 'public, max-age=' + ttl_cache);
+    res.send(result);
+  });
 });
 
 var port = Number(process.env.PORT || 3000);
