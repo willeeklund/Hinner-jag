d3fa9be9983fdc1c7cc793595f736189e4612aa6
Wilhelm Eklund <wille.eklund@gmail.com>
2016-05-10 21:49:58 +0200

    Quickfix for backend

--- backend/stationInfo.js
+++ backend/stationInfo.js
@@ -6,7 +6,7 @@ var fs = require('fs'),
     stationsFileContent = fs.readFileSync(fileName),
     stationData = JSON.parse(stationsFileContent);
 
-var sections = ['metro_and_train_stations', 'metro_stations', 'train_stations'];
+var sections = ['METROSTN', 'RAILWSTN', 'TRAMSTN', 'FERRYBER', 'BUSTERM'];
 
 // Whitelist for allowed site ids
 var whitelistSiteId = [];
@@ -33,9 +33,11 @@ var endStationSiteIdList = [
 ];
 
 sections.forEach(function (section) {
-    stationData[section].forEach(function (item) {
-        whitelistSiteId.push(item.siteid);
-    });
+    if (stationData[section]) {
+        stationData[section].forEach(function (item) {
+            whitelistSiteId.push(item.siteid);
+        });
+    }
 });
 
 var isEndStation = function (siteId) {
